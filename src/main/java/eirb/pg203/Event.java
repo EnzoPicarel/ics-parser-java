a92a760c5aa5f606ecbf2b16920f72d535837c7b
Thibault Abeille <thibault.abeille@enseirb-matmeca.fr>
2025-11-06 12:21:36 +0100

    class Event

---
+++ src/main/java/eirb/pg203/Event.java
@@ -0,0 +1,27 @@
+package eirb.pg203;
+
+public enum Attendance {
+    PUBLIC, CONFIDENTIAL;
+}
+
+class Event {
+    
+    Instant creation_date;
+    Instant start_date;
+    Instant end_date;
+    String summary;
+    String location;
+    String description;
+    Attendance attendance;
+
+
+    Event(Instant creation_date, Instant start_date, Instant end_date, String summary, String location, String description, Attendance attendance) {
+        this.creation_date = creation_date;
+        this.start_date = start_date;
+        this.end_date = end_date;
+        this.summary = summary;
+        this.location = location;
+        this.description = description;
+        this.attendance = attendance;
+    }
+}
